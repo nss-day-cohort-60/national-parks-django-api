d95eb787d0c548225a01142aa232af937290065c
Hazel Preza <hazel.d.preza@gmail.com>
2023-02-13 14:14:21 -0600

    made 3 more models than i said i would

---
+++ parksapi/models/__init__.py
@@ -0,0 +1,6 @@
+from .blog import Blog
+from .blog_favorite import BlogFavorite
+from .event_favorite import EventFavorite
+from .park_favorite import ParkFavorite
+from .photo_favorite import PhotoFavorite
+from .photo import Photo
