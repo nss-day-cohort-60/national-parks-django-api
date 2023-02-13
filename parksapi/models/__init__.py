a639f9089ed6935c46f205e77db1680ab1b0becc
Vanessa Spear <vanessavspear@gmail.com>
2023-02-13 14:41:47 -0600

    Changed model names from Pascal to snake case.

--- parksapi/models/__init__.py
+++ parksapi/models/__init__.py
@@ -1,17 +1,14 @@
-<<<<<<< Updated upstream
-from .NaturalAttraction import NaturalAttraction
-from .ParkNaturalAttraction import ParkNaturalAttraction
-from .ParkWildlife import ParkWildlife
+from .natural_attraction import NaturalAttraction
+from .park_natural_attraction import ParkNaturalAttraction
+from .park_wildlife import ParkWildlife
 from .blog import Blog
 from .blog_favorite import BlogFavorite
 from .event_favorite import EventFavorite
 from .park_favorite import ParkFavorite
 from .photo_favorite import PhotoFavorite
 from .photo import Photo
-=======
 from .event import Event
 from .campground import Campground
 from .camping_reservation import CampingReservation
-from event_registration import EventRegistration
+from .event_registration import EventRegistration
 
->>>>>>> Stashed changes
